99bb0f0d752227085af2556800a24b6ab297598b
Andres Valderrama <_andresvalderrama@outlook.com>
2017-11-13 23:50:23 -0500

    i18n
    Adding support for locate error from validator and return given values to the client

--- app/signin/controller.js
+++ app/signin/controller.js
@@ -1,10 +1,12 @@
 exports.render = function (req, res) {
   let errors = req.flash('error')[0] || []
+  let clientValues = req.flash('values')[0] || []
 
   res.render('signin/template', {
     title: 'Signin',
+    valueUsermail: !errors.usermail ? clientValues.usermail : undefined,
     errorUsermail: errors.usermail ? errors.usermail.msg : undefined,
-    errorPassword: errors.password ? errors.password.msg : undefined
+    errorPassword: !errors.usermail && errors.password ? errors.password.msg : undefined
   })
 }
 
--- config/validators.js
+++ config/validators.js
@@ -6,13 +6,26 @@ exports.signin = [
     .trim()
     .normalizeEmail(),
   check('password', 'password must be at least 5 chars long and contain one number')
+    .trim()
     .isLength({ min: 5 })
     .matches(/\d/),
 
   (req, res, next) => {
     const errors = validationResult(req)
+    const i18nCatalog = req.getCatalog()
+
     if (!errors.isEmpty()) {
-      req.flash('error', validationResult(req).mapped())
+      let mappedErrors = validationResult(req).mapped()
+
+      if (mappedErrors.usermail) {
+        mappedErrors.usermail.msg = i18nCatalog.singInP.userMailError
+      }
+      if (mappedErrors.password) {
+        mappedErrors.password.msg = i18nCatalog.singInP.passwordError
+      }
+
+      req.flash('error', mappedErrors)
+      req.flash('values', req.body)
     }
     next()
   }
