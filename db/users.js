afddfe1d4f855b19dacf5577104379693db29b35
Andres Valderrama <_andresvalderrama@outlook.com>
2017-11-13 23:51:30 -0500

    User model
    change password for fake user model

--- db/users.js
+++ db/users.js
@@ -2,8 +2,8 @@ module.exports = [
   {
     id: 'h452vh5g24j5h',
     user: 'andresvalderrama',
-    email: '_andresvalderrama',
-    password: '123456'
+    email: '_andresvalderrama@outlook.com',
+    password: '1234590'
   },
   {
     id: 'h74l6h73kh4ec',
