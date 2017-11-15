9df58b4430b2c4593d4c74051c62b721b7fdc9f2
Andres Valderrama <_andresvalderrama@outlook.com>
2017-11-15 12:02:54 -0500

    Amnesia - csrf
    add support for csrf and show a diferent markup when success var is set to true

--- app/amnesia/controller.js
+++ app/amnesia/controller.js
@@ -1,11 +1,14 @@
 exports.checkEmail = (req, res, next) => {
   console.log('check email exists in the db')
-  return next()
+
+  res.redirect('/amnesia')
 }
 
 exports.render = (req, res) => {
   res.render('amnesia/template', {
     title: 'Amnesia',
-    redirectWhenLocale: '/amnesia'
+    redirectWhenLocale: '/amnesia',
+    csrfToken: req.csrfToken(),
+    success: false
   })
 }
--- app/amnesia/router.js
+++ app/amnesia/router.js
@@ -1,8 +1,22 @@
 const router = require('express').Router()
+const csrfProtection = require('../../config/csrf')
 
 const amnesiaCtrl = require('./controller')
 const amnesiaVldtr = require('./validator')
 
-router.get('/amnesia', amnesiaVldtr.validate, amnesiaCtrl.render)
+/*
+ * el fomulario debe traer un email
+ * se ejecuta el middleware csrfProtection
+ *  false ???
+ * se valida si es un email
+ *  false -> se redirecciona a GET /amnesia con error
+ * se consulta el email
+ *  false -> se redirecciona a GET /amnesia con error
+ * se redirecciona a GET /amnesia con var-success
+ *
+ * */
+
+router.get('/amnesia', csrfProtection, amnesiaCtrl.render)
+router.post('/amnesia', csrfProtection, amnesiaVldtr.validate, amnesiaCtrl.checkEmail)
 
 module.exports = router
