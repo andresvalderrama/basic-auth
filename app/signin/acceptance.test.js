704651350c364d6f80277a71a770c9338f68efb8
Andres Valderrama <_andresvalderrama@outlook.com>
2017-12-12 15:20:23 -0500

    Change port on functional test

--- app/signin/acceptance.test.js
+++ app/signin/acceptance.test.js
@@ -3,7 +3,7 @@ const Browser = require('zombie')
 
 // We're going to make requests to http://example.com/signup
 // Which will be routed to our test server localhost:3000
-Browser.localhost('localhost', 3333)
+Browser.localhost('localhost', 3000)
 
 describe('User visits signup page', () => {
   const browser = new Browser()
